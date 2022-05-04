6f1089cfd1ffa3ff798684a5c3394f205c6241cf
Marvin Lee Fimmel <kontakt@ekranos.me>
2022-05-04 15:06:56 +0200

    Added timeout to check_ssl_ng Some minor improvements in check_ssl_ng

--- check_ssl_ng/src/main.rs
+++ check_ssl_ng/src/main.rs
@@ -2,8 +2,9 @@ use anyhow::Context;
 use chrono::{Local, NaiveDateTime};
 use nagiosplugin::{CheckResult, Metric, Resource, ServiceState, TriggerIfValue};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
-use std::net::TcpStream;
+use std::net::{TcpStream, ToSocketAddrs};
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -19,11 +20,11 @@ struct Opts {
 
     /// Time in days until expiry to report a warning
     #[structopt(long, default_value = "30")]
-    warning: i32,
+    warning: u64,
 
     /// Time in days until expiry to report a critical
     #[structopt(long, default_value = "14")]
-    critical: i32,
+    critical: u64,
 
     /// Service state to emit if the certificate is not valid yet
     #[structopt(long, default_value = "critical")]
@@ -32,6 +33,10 @@ struct Opts {
     /// If set, the check will expect the address to not respond and will issue the given state if it does respond
     #[structopt(long)]
     expect_error: Option<ServiceState>,
+
+    /// The timeout in milliseconds
+    #[structopt(long, default_value = "15000")]
+    timeout: u64,
 }
 
 struct CertificateInfo {
@@ -89,8 +94,16 @@ fn do_check(opts: &Opts) -> anyhow::Result<Resource> {
 
     let connector = builder.build();
 
-    let stream = TcpStream::connect(&opts.address)
+    let addr = opts
+        .address
+        .to_socket_addrs()
+        .with_context(|| "invalid address")?
+        .next()
+        .with_context(|| "no address found")?;
+
+    let stream = TcpStream::connect_timeout(&addr, Duration::from_millis(opts.timeout))
         .with_context(|| format!("error connecting to {}", &opts.address));
+
     let stream = match (stream, opts.expect_error) {
         (Ok(_), Some(state)) => {
             resource.set_description("Expected remote to be unreachable, but it is reachable.");
@@ -121,7 +134,7 @@ fn do_check(opts: &Opts) -> anyhow::Result<Resource> {
 
     let now = Local::now().naive_local();
     {
-        let left_days = (last_cert.not_after - now).num_days() as i32;
+        let left_days = (last_cert.not_after - now).num_days() as u64;
         resource.push_result(Metric::new("not_after", left_days).with_thresholds(
             opts.warning,
             opts.critical,
